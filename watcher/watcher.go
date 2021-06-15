35ef7e88486a1d60590565326aa5d206831537c5
barneyzhong <barney@barneyzhongdeMacBook-Pro.local>
2021-06-15 17:24:28 +0800

    modify 1

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -114,10 +114,12 @@ func (w *Watcher) Run(ctx context.Context) error {
 			payload.Ingresses = append(payload.Ingresses, ingressPayload)
 
 			for _, rule := range ingress.Spec.Rules {
-				klog.Infof("准备开始组装数据")
-				if rule.HTTP != nil {
+				
+				if rule.HTTP == nil {
+					klog.Infof("数据为空，不组装")
 					continue
 				}
+				klog.Infof("准备开始组装数据")
 				for _, path := range rule.HTTP.Paths {
 					// 给 ingressPayload 组装数据
 					addBackend(&ingressPayload, rule.Host, path.Path, path.Backend)
